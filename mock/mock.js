afb0976923b1dfd70d5fbe46a2cdfaab075c9903
Jack Liu <liuming@mzsat.cn>
2020-01-14 20:45:09 +0800

    init

---
+++ mock/mock.js
@@ -0,0 +1,11 @@
+const swiper = [{
+	image: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1579606809&di=e6fbb2a165b4927a7b646db31a3141dc&imgtype=jpg&er=1&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180615%2F61b29be3cd3b47d7ae60b956eca56a85.jpeg',
+	link: ''
+}];
+const kaotuanList = [{
+	title: '洛杉矶考团',
+	image:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1579606809&di=e6fbb2a165b4927a7b646db31a3141dc&imgtype=jpg&er=1&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180615%2F61b29be3cd3b47d7ae60b956eca56a85.jpeg',
+	desc:'洛杉矶考团是明志考团的优势项目，北京或上海直飞洛杉矶，入驻酒店为希尔顿，小考场环境优雅，离酒店仅15分钟车程。如没有美国签证可联系明志协助办理。',
+	note:['六天五夜','查看详情']
+}];
+export default {swiper,kaotuanList}
---
+++ pages/index/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="(item,index) in swiper" :key='index'>
+				<view class="swiper-item" >
+					<image :src="item.image"></image>
+				</view>
+			</swiper-item>
+			
+		</swiper>
+		<view v-for="(item,index) in kaotuanList" :key='index'>
+			<uni-card  mode='style' :title='item.title' note='true' :extra='item.desc' :thumbnail='item.image' >
+			<template v-slot:footer>
+				<view class="uni-card-footer">
+					<view class="uni-card-text" v-for='(childItem,childIndex) in item.note' :key='childIndex'>
+						{{childItem}}
+					</view>
+				</view>
+			</template>
+			</uni-card>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	import {uniCard} from '@dcloudio/uni-ui';
+	import mock from '../../mock/mock.js';
+	export default {
+		components:{uniCard},
+		data() {
+			return {
+				swiper:[],
+				kaotuanList:[],
+			}
+		},
+		onLoad() {
+			console.log(mock)
+			this.swiper=mock.swiper;
+			this.kaotuanList=mock.kaotuanList;
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.uni-card-image{
+		width: 100%;
+	}
+	.uni-card-footer{
+		display: flex;
+		justify-content: space-between;
+	}
+	.uni-card-text{
+		font-size: 24upx;
+	}
+	
+</style>
